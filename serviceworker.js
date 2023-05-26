2abee79687290a90f22a98249622b7639c421fcc
Omar Ochoa <omar@ochoawebdesign.com>
2023-05-25 20:51:29 -0700

    Creates serviceworker with simple cache

---
+++ serviceworker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("pwa-assets").then(cache => {
+            return cache.addAll([
+                "./", "main.js", 
+                "/img/icon-48.png", 
+                "/img/icon-96.png",  
+                "/img/icon-256.png", 
+                "/img/icon-512.png",
+            ]);
+        })
+    ); 
+ });
+
+ self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+ });
