077671ef0417f8eb2609b775fab5e42cd71b6f09
Omar Ochoa <omar@ochoawebdesign.com>
2023-05-25 22:45:29 -0700

    Put form.js into main.js

--- main.js
+++ main.js
@@ -8,5 +8,21 @@ if ("serviceWorker" in navigator) {
     });
 }
 
+// Login Form Logic
+
+let loginForm = document.querySelector("#login");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+  
+    if (username.value == "" || password.value == "") {
+      alert("Ensure you input a value in both fields!");
+    } else {
+      // perform operation with form input
+      window.location.href = "./main.html";
+    }
+  });
 
 
